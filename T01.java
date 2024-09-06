e40dc40ab84773a6a86043b9ffd761b5c17d2895
LucasPardede <lucaspardede184@gmail.com>
2024-09-06 11:36:28 +0700

    TUGAS SUDAH SELESAI PAIR 21

--- T01.java
+++ T01.java
@@ -1,1 +1,35 @@
-// NIM - Your Name
+// 12S24022 - INGRATE JOY SIHOMBING
+// 12S24015 - LUCAS PARDEDE
+
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String jUDUL;
+        String pENULIS;
+        int tAHUNTERBIT;
+        String pENERBIT;
+        String fORMATBUKU;
+        double hARGAPEMBELIAN;
+        double mINIMUMMARGIN;
+        int sTOK;
+        double rATING;
+
+        iSBN = input.nextLine();
+        jUDUL = input.nextLine();
+        pENULIS = input.nextLine();
+        tAHUNTERBIT = input.nextInt();
+        pENERBIT = input.nextLine();
+        fORMATBUKU = input.nextLine();
+        hARGAPEMBELIAN = input.nextDouble();
+        mINIMUMMARGIN = input.nextDouble();
+        sTOK = input.nextInt();
+        rATING = input.nextDouble();
+        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + tAHUNTERBIT + "|" + pENERBIT + "|" + fORMATBUKU + "|" + hARGAPEMBELIAN + "|" + mINIMUMMARGIN + "|" + sTOK + "|" + rATING);
+    }
+}
